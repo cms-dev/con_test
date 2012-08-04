9509e0d9fb196b24d79372ca737df64bc5078d38
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2012-08-04 11:45:40 +0200

    Fix C and C++ stubs for communication.

--- communication/cor/stub.c
+++ communication/cor/stub.c
@@ -1,13 +1,23 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int add(int a, int b);
 
-int main() {
+int main(int argc, char **argv) {
+
+	FILE *fifo_in, *fifo_out;
+	fifo_in = fopen(argv[2], "r");
+	fifo_out = fopen(argv[1], "w");
 
 	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d\n", add(a, b));
+	fscanf(fifo_in, "%d %d", &a, &b);
+	fprintf(fifo_out, "%d\n", add(a, b));
+	fflush(fifo_out);
+
+	fclose(fifo_in);
+	fclose(fifo_out);
+
 	return 0;
 
 }
